f6bcff9e8106e9abfe75f2a0846cec220e72804d
nfrd <vincent.nafrada@gmail.com>
2024-07-28 12:06:18 -0400

    feat(attributes): add attributes.js, separated business logic from view

---
+++ src/attributes.js
@@ -0,0 +1,26 @@
+// attributes.js
+export const initialAttributes = {
+    'Strength': 10,
+    'Dexterity': 10,
+    'Constitution': 10,
+    'Intelligence': 10,
+    'Wisdom': 10,
+    'Charisma': 10,
+};
+
+export const incrementAttribute = (attributes, setAttributes, attribute) => {
+    setAttributes((prev) => ({
+        ...prev,
+        [attribute]: prev[attribute] + 1,
+    }));
+};
+
+export const decrementAttribute = (attributes, setAttributes, attribute) => {
+    if (attributes[attribute] > 0) {
+        setAttributes((prev) => ({
+            ...prev,
+            [attribute]: prev[attribute] - 1,
+        }));
+    }
+};
+
